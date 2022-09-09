30e71427ed03905f615146deb12d885703eda02e
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-09-09 09:37:02 +0300

    create student database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,16 @@ s2 = School.create({school_name:'Data Africa', address:'3526 Mombasa', phone: '0
 s3 = School.create({school_name:'Udemy', address:'8787 Eldoret', phone: '0788883333'})
 s4 = School.create({school_name:"Dalworth", address:'675 Nairobi', phone: '0788883333'})
 
-# p1 = Student.create({gender:"male", student_name:"Maurice Nganga", age:"26", phone: "0723675634", school_id: s1.id})
-# p2 = Student.create({gender:"male", student_name:"Tracey Lugahi", age:"23", phone: "0767998634", school_id: s2.id})
-# p3 = Student.create({gender:"female", student_name:"Sarah Mukuti", age:"25", phone: "0725634900", school_id: s2.id})
-# p4 = Student.create({gender:"female", student_name:"Amos Koech", age:"24", phone: "0756775634", school_id: s1.id})
-# p5 = Student.create({gender:"male", student_name:"Victor Mmula", age:"24", phone: "0713685634", school_id: s3.id})
-# p6 = Student.create({gender:"female", student_name:"James Chemobo", age:"26", phone: "0733605634", school_id: s1.id})
-# p7 = Student.create({gender:"male", student_name:"Billy Mwangi", age:"23", phone: "0723909634", school_id: s3.id})
-# p8 = Student.create({gender:"female", student_name:"Tabitha Chepngetich", phone: "0720098634", age:"26", school_id: s4.id})
-# p9 = Student.create({gender:"female", student_name:"Bernad Ngari", age:"22", phone: "07290885634", school_id: s1.id})
-# p10 = Student.create({gender:"male", student_name:"Bryan Sagini", age:"25", phone: "0798766756", school_id: s4.id})
+p1 = Student.create({gender:"male", student_name:"Maurice Nganga", age:"26", phone: "0723675634", school_id: s1.id})
+p2 = Student.create({gender:"male", student_name:"Tracey Lugahi", age:"23", phone: "0767998634", school_id: s2.id})
+p3 = Student.create({gender:"female", student_name:"Sarah Mukuti", age:"25", phone: "0725634900", school_id: s2.id})
+p4 = Student.create({gender:"female", student_name:"Amos Koech", age:"24", phone: "0756775634", school_id: s1.id})
+p5 = Student.create({gender:"male", student_name:"Victor Mmula", age:"24", phone: "0713685634", school_id: s3.id})
+p6 = Student.create({gender:"female", student_name:"James Chemobo", age:"26", phone: "0733605634", school_id: s1.id})
+p7 = Student.create({gender:"male", student_name:"Billy Mwangi", age:"23", phone: "0723909634", school_id: s3.id})
+p8 = Student.create({gender:"female", student_name:"Tabitha Chepngetich", phone: "0720098634", age:"26", school_id: s4.id})
+p9 = Student.create({gender:"female", student_name:"Bernad Ngari", age:"22", phone: "07290885634", school_id: s1.id})
+p10 = Student.create({gender:"male", student_name:"Bryan Sagini", age:"25", phone: "0798766756", school_id: s4.id})
 
 
 puts "✅ Done seeding!"
