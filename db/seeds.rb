cfa94c4a7a8b1e6fa5a238124a246d4ef7383785
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-09-09 09:35:03 +0300

    create school database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-# puts "🌱 Seeding data..."
+puts "🌱 Seeding data..."
 
-# # Seed your database here
-# s1 = School.create({school_name:'Moringa School', address:'11 Nairobi', phone: '0724306223'})
-# s2 = School.create({school_name:'Data Africa', address:'3526 Mombasa', phone: '0724809888'})
-# s3 = School.create({school_name:'Udemy', address:'8787 Eldoret', phone: '0788883333'})
-# s4 = School.create({school_name:"Dalworth", address:'675 Nairobi', phone: '0788883333'})
+# Seed your database here
+s1 = School.create({school_name:'Moringa School', address:'11 Nairobi', phone: '0724306223'})
+s2 = School.create({school_name:'Data Africa', address:'3526 Mombasa', phone: '0724809888'})
+s3 = School.create({school_name:'Udemy', address:'8787 Eldoret', phone: '0788883333'})
+s4 = School.create({school_name:"Dalworth", address:'675 Nairobi', phone: '0788883333'})
 
 # p1 = Student.create({gender:"male", student_name:"Maurice Nganga", age:"26", phone: "0723675634", school_id: s1.id})
 # p2 = Student.create({gender:"male", student_name:"Tracey Lugahi", age:"23", phone: "0767998634", school_id: s2.id})
