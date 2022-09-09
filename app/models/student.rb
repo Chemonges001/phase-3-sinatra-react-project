bbb933545dd5f7a1b8f961fbb07707b1dcdb9ff1
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-09-09 09:55:59 +0300

    complete back-end

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,17 +1,8 @@
 class Student < ActiveRecord::Base
     belongs_to :school
     
-    # def returnName
-    #     "#{self.student_name}-#{self.age}"
-    # end
+    def returnName
+        "#{self.student_name}"
+    end
 
-    # def print 
-    #     puts "Name: #{self.student_name}"
-    #     puts "Age: #{self.age}"
-    #     puts "Gender: #{self.gender}"
-    #     puts "Phone number #{self.phone}"
-    #     self.student_name
-    # end 
-
-    # newValue = print()
 end
