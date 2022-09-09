be464d043397329c585661a0898563bdeda3b774
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-09-09 09:22:10 +0300

    create migration

--- db/migrate/20220908231449_create_students.rb
+++ db/migrate/20220908231449_create_students.rb
@@ -1,10 +1,10 @@
-# class CreateStudents < ActiveRecord::Migration[6.1]
-#   def change
-#     create_table :schools do |t|
-#       t.string :school_name
-#       t.string :address
-#       t.string :phone
-#       t.timestamps 
-#     end
-#   end
-# end
+class CreateStudents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :schools do |t|
+      t.string :school_name
+      t.string :address
+      t.string :phone
+      t.timestamps 
+    end
+  end
+end
